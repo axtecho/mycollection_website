97c5cb0ed2971b9d19bcf7ff12f01c15de5377ef
Lasse Joergensen <lass876q@stud.kea.dk>
2021-10-05 21:09:31 +0200

    Implemented grid, +maincontent+filternav+grids

--- js/js.js
+++ js/js.js
@@ -4,14 +4,24 @@ document.querySelector(".open-slide").addEventListener("click", openSlideMenu);
 
 function openSlideMenu() {
   document.getElementById("side-menu").style.width = "300px";
-
-  document.getElementById("main").style.paddingRight = "300px";
 }
 
 document.querySelector("#close").addEventListener("click", closeSlideMenu);
 
 function closeSlideMenu() {
   document.getElementById("side-menu").style.width = "0";
+}
 
-  document.getElementById("main").style.paddingRight = "0";
+/* ----------------FILTERS MENU---------------------------- */
+
+document.querySelector(".filter").addEventListener("click", openFilterMenu);
+
+function openFilterMenu() {
+  document.getElementById("filterjs").style.width = "200px";
+}
+
+document.querySelector("#closetoo").addEventListener("click", closeFiltermenu);
+console.log("hola");
+function closeFiltermenu() {
+  document.getElementById("filterjs").style.width = "0";
 }
